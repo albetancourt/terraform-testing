865c8d7f09224e0361f862e3f4dbbfda87532b96
albetancourt <betancourt.al@gmail.com>
2023-11-27 10:14:25 -0300

    get modified folders

---
+++ tools/get-modified-module-folders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GH_DIFF_OUTPUT=$(cat << 'EOF'
+infra/modules/security/main.tf
+infra/dev/main.tf
+services/payment/modules/notification/variables.tf
+infra/modules/security/variables.tf
+services/payment/payment.go
+infra/modules/module-01/main.tf
+EOF
+)
+
+echo "$GH_DIFF_OUTPUT" | grep -oP '^((infra/modules/.*)(?=/))|((services/.*/modules/.*)(?=/))' | sort | uniq
